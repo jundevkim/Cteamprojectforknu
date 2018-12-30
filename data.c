d40d5ac31d64630b27cdcec7f304705204991987
junhardstudy <junhardstudy@knu.ac.kr>
2018-12-30 23:02:53 +0900

    working

--- data.c
+++ data.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
-void inputData() {
+void storeData() {
+	char tempFood[20];
+	int i;
+	struct food {
+		char food1[20], food2[20], food3[20];
+	};
+
+	struct food bread;
+
+	for (i = 0; i < 3; i++) {
+		scanf("%s", bread.food1);
+	}
 
 }
